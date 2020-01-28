e96198e1ede5c9ff3af2df26c3b4b35e83bb62d3
sanderdebr <sander606@gmail.com>
2020-01-28 07:30:05 +0100

    stuck on d3

--- src/components/Chart/index.jsx
+++ src/components/Chart/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 import * as d3 from 'd3'
 import _ from 'lodash';
+import './chart.css';
 
 function Chart(props) {
 
@@ -16,51 +17,54 @@ function Chart(props) {
 
         const { activeDays } = chartData;
 
-        const activeDaysAmount = Object.values(_.countBy(activeDays));
+        const activeDaysData = _.countBy(activeDays);
 
-        const height = 200;
-        const width = '100%';
+        console.log(activeDays)
+
+        const activeDaysAmount = Object.values(activeDaysData);
+
+        const height = 300;
+        const width = 300;
 
         // Clear previous chart
         canvas.current.innerHTML = "";
 
-        // Append SVG 
         const svg = d3.select(canvas.current)
             .append("svg")
             .attr("width", width)
             .attr("height", height);
 
-        // Create scale
-        let scale = d3.scaleLinear()
-            .domain([0, daysInMonth])
-            .range([0, canvas.current.offsetWidth - 100]);
-
-        // Add scales to axis
-        const x_axis = d3.axisBottom()
-            .scale(scale);
-
+        // Add X axis --> it is a date format
+        var x = d3.scaleLinear()
+            .domain(d3.extent(activeDaysData, function(d) { return d[0]; }))
+            .range([ 0, width ]);
         svg.append("g")
-           .attr("transform", "translate(35, 120)")
-           .call(x_axis);
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
 
-        // Set y axis min = 0 and max = maximum day activities
-
-        scale = d3.scaleLinear()
-                    .domain([d3.min(activeDaysAmount), d3.max(activeDaysAmount)])
-                    .range([height/2, 0]);
-
-        const y_axis = d3.axisLeft()
-                    .scale(scale);
-
+        // Add Y axis
+        var y = d3.scaleLinear()
+            .domain([0, d3.max(activeDaysData, function(d) { return +d.value; })])
+            .range([ height, 0 ]);
         svg.append("g")
-           .attr("transform", "translate(30, 10)")
-           .call(y_axis);
-
+            .call(d3.axisLeft(y));
 
         // Set points for each day per activity
+        
+        // Add the line
+        svg.append("path")
+            .datum(activeDaysData)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+            .x(function(d) { return x(d.date) })
+            .y(function(d) { return y(d.value) })
+            )
+
     }
 
-    return <div ref={canvas}></div>
+    return <div className="chart" ref={canvas}></div>
 }
 
 export default Chart;
